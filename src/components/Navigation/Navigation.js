3f85d3e7dea605487175652a4b8b57ca0c715cac
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2019-09-14 16:52:59 +0700

    Add nav to hide on scroll

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
@@ -58,53 +58,96 @@ NavLink.propTypes = {
   to: PropTypes.string.isRequired,
 }
 
-const Navigation = ({ ...props }) => {
-  return (
-    <Box
-      bg={theme.colors.white}
-      py={[2]}
-      css="z-index: 999; position: fixed; top: 0; left: 0; width: 100%"
-    >
-      <DefaultLayout>
-        <Flex
-          as="nav"
-          alignItems="center"
-          justifyContent="space-between"
-          css="position: relative"
+const Transition = styled.div`
+  .active {
+    visibility: visible;
+    transition: all 200ms ease-in-out;
+  }
+  .hidden {
+    visibility: hidden;
+    transition: all 200ms ease-in-out;
+    transform: translate(0, -100%);
+  }
+`
+
+export default class Navigation extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: true,
+      scrollPos: 0,
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll() {
+    const { scrollPos } = this.state
+    this.setState({
+      scrollPos: document.body.getBoundingClientRect().top,
+      show: document.body.getBoundingClientRect().top > scrollPos,
+    })
+  }
+
+  render() {
+    return (
+      <Transition>
+        <Box
+          bg={theme.colors.white}
+          py={[2]}
+          css="z-index: 999; position: fixed; top: 0; left: 0; width: 100%"
+          className={this.state.show ? "active" : "hidden"}
         >
-          <SkipNavLink />
+          <DefaultLayout>
+            <Flex
+              as="nav"
+              alignItems="center"
+              justifyContent="space-between"
+              css="position: relative"
+            >
+              <SkipNavLink />
 
-          <Flex alignItems="center">
-            <Logo />
-          </Flex>
+              <Flex alignItems="center">
+                <Logo />
+              </Flex>
 
-          <List fontSize={[2]}>
-            <NavLink to="/" mr={[2, 4]}>
-              Works
-            </NavLink>
+              <List fontSize={[2]}>
+                <NavLink to="/" mr={[2, 4]}>
+                  Works
+                </NavLink>
 
-            <NavLink to="/about/" mr={[2, 4]}>
-              About
-            </NavLink>
+                <NavLink to="/about/" mr={[2, 4]}>
+                  About
+                </NavLink>
 
-            <NavLink to="/#talks" mr={[2, 4]}>
-              Talks
-            </NavLink>
+                <NavLink to="/#talks" mr={[2, 4]}>
+                  Talks
+                </NavLink>
 
-            <NavLink to="/writing/" mr={[2, 4]}>
-              Writing
-            </NavLink>
+                <NavLink to="/writing/" mr={[2, 4]}>
+                  Writing
+                </NavLink>
 
-            <a href="mailto:afnizarhilmi@gmail.com">
-              <Button css="padding: 0.75rem 1rem!important" variant="primary">
-                Contact
-              </Button>
-            </a>
-          </List>
-        </Flex>
-      </DefaultLayout>
-    </Box>
-  )
+                <a href="mailto:afnizarhilmi@gmail.com">
+                  <Button
+                    css="padding: 0.75rem 1rem!important"
+                    variant="primary"
+                  >
+                    Contact
+                  </Button>
+                </a>
+              </List>
+            </Flex>
+          </DefaultLayout>
+        </Box>
+      </Transition>
+    )
+  }
 }
-
-export default Navigation
