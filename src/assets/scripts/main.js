1693d679b7ff753b75cfa942a059b924ad844a98
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2022-01-07 17:26:53 +0700

    add footer scroll

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,6 +1,7 @@
 // Focus Visible Polyfill
-import 'focus-visible'
+import "focus-visible"
 
 // Internal Modules
-import './modules/nav'
-import './modules/themepicker'
+import "./modules/nav"
+import "./modules/themepicker"
+import "./modules/onscroll"
