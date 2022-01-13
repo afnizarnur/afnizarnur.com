0f333b17efb23d8cbd1f5391a946f090666744db
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2022-01-13 23:22:18 +0700

    add scroll change background color

--- src/assets/scripts/animation/animation.home.js
+++ src/assets/scripts/animation/animation.home.js
@@ -2,6 +2,12 @@ import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
 
 const HomeAnimation = () => {
+    const el = {
+        dt: document.querySelector(".designtooling"),
+        sw: document.querySelector(".selectedwork"),
+        c: document.querySelector("body")
+    }
+
     gsap.registerPlugin(ScrollTrigger)
 
     const tl = gsap.timeline({
@@ -16,6 +22,34 @@ const HomeAnimation = () => {
     }).to(".aboutme", {
         opacity: 1
     })
+
+    const tlline = gsap
+        .timeline({
+            scrollTrigger: {
+                trigger: ".designtooling h2",
+                scrub: true,
+                start: "top bottom",
+                end: "top bottom-=200px"
+            }
+        })
+        .to(
+            el.c,
+            {
+                duration: 1,
+                backgroundColor: "var(--background-default)",
+                ease: "Power3.easeInOut"
+            },
+            0
+        )
+        .to(
+            el.c,
+            {
+                duration: 1,
+                backgroundColor: "var(--surface-primary-inverse)",
+                ease: "Power3.easeInOut"
+            },
+            0
+        )
 
     return tl
 }
