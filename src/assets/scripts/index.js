04b1dc7f375266d09cccaafcf872c59e2e3beee9
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2022-02-19 20:14:38 +0700

    errand things

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -2,7 +2,7 @@ import LoaderAnimation from "./animation/animation.loader"
 import HomeAnimation from "./animation/animation.home"
 
 const handleInitialLoad = (e) => {
-    // LoaderAnimation().play()
+    LoaderAnimation().play()
 
     if (document.querySelector(".aboutme")) {
         HomeAnimation()
