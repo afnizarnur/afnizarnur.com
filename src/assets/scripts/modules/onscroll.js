d5ee5cd13f1cc57fcc25c672d03aede4c7e7c631
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-14 22:04:52 +0700

    Refactor

--- src/assets/scripts/modules/onscroll.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function () {
-    var nav = document.querySelector(".header__inner")
-
-    if (window.pageYOffset > 208) {
-        nav.classList.add("onscroll")
-    } else {
-        nav.classList.remove("onscroll")
-    }
-}
