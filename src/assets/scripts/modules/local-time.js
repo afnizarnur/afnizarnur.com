283bec029ab9d7ca8fb44c221c88cb41a4278882
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-14 21:58:29 +0700

    Add local time functionality

---
+++ src/assets/scripts/modules/local-time.js
@@ -0,0 +1,22 @@
+class LocalTime {
+    constructor(offset, elementId) {
+        this.offset = offset
+        this.element = document.getElementById(elementId)
+        this.updateLocalTime()
+        setInterval(this.updateLocalTime.bind(this), 1000)
+    }
+
+    updateLocalTime() {
+        const now = new Date()
+        const utcHours = now.getUTCHours()
+        const hours = ("0" + ((utcHours + this.offset / 60) % 24)).slice(-2)
+        const minutes = ("0" + now.getMinutes()).slice(-2)
+
+        const formattedTime = hours + ":" + minutes
+        if (this.element) {
+            this.element.textContent = formattedTime
+        }
+    }
+}
+
+const localtime = new LocalTime(420, "local-time")
