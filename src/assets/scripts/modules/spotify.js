3ab97416923f3c8d63e5fe5fdeef6bc9405d2e3f
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-15 11:26:46 +0700

    Fix api spotify

--- src/assets/scripts/modules/spotify.js
+++ src/assets/scripts/modules/spotify.js
@@ -1,66 +1,34 @@
-import axios from "axios"
-
-async function getRecentlyPlayedTrack() {
-    const accessToken = process.env.SPOTIFY_ACCESS_TOKEN
-
-    const headers = {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json"
+class SpotifyFetcher {
+    constructor() {
+        this.paragraph = document.getElementById("current-song")
+        this.fetchSpotify()
     }
 
-    try {
-        const response = await axios.get(
-            "https://api.spotify.com/v1/me/player/recently-played?limit=1",
-            {
-                headers: headers
-            }
-        )
+    fetchSpotify() {
+        fetch("/.netlify/functions/spotify")
+            .then((res) => res.json())
+            .then((data) => {
+                const songTitle = data.name
+                const artistName = data.artists[0].name
+                const url = data.url
+                this.updateParagraph(songTitle, artistName, url)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
 
-        if (response.status !== 200) {
-            if (!process.env.ELEVENTY_ENV === "production") {
-                throw new Error(
-                    "Failed to fetch recently played track from Spotify API"
-                )
-            }
-        }
-
-        const data = response.data
-        if (data.items.length === 0) {
-            return null
-        }
-
-        const track = data.items[0].track
-        const songTitle = track.name
-        const artistName = track.artists[0].name
-        return { songTitle, artistName }
-    } catch (error) {
-        if (!process.env.ELEVENTY_ENV === "production") {
-            throw new Error(
-                "Failed to fetch recently played track from Spotify API"
-            )
-        }
+    updateParagraph(songTitle, artistName, url) {
+        this.paragraph.innerHTML = `
+        <div class="equalizer-container">
+          <div class="equalizer-bar"></div>
+          <div class="equalizer-bar"></div>
+          <div class="equalizer-bar"></div>
+        </div>
+        <a href="${url}" target="_blank">${songTitle} by ${artistName}</a>`
     }
 }
 
-async function displayCurrentSong() {
-    try {
-        const trackInfo = await getRecentlyPlayedTrack()
-        if (trackInfo) {
-            const { songTitle, artistName } = trackInfo
-            const paragraph = document.getElementById("current-song")
-            paragraph.innerHTML = `
-                <div class="equalizer-container">
-                    <div class="equalizer-bar"></div>
-                    <div class="equalizer-bar"></div>
-                    <div class="equalizer-bar"></div>
-                </div>
-                <a href="https://open.spotify.com/user/afnizarnur" target="_blank">${songTitle} by ${artistName}</a>`
-        }
-    } catch (error) {
-        if (!process.env.ELEVENTY_ENV === "production") {
-            console.error(error)
-        }
-    }
-}
-
-document.addEventListener("DOMContentLoaded", displayCurrentSong)
+document.addEventListener("DOMContentLoaded", () => {
+    new SpotifyFetcher()
+})
