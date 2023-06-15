1925eda6a85b2bf3bba7ba1e4b2a7729011ec08a
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-15 14:25:56 +0700

    add hash commit

--- src/data/build.js
+++ src/data/build.js
@@ -1,4 +1,18 @@
-module.exports = {
-    env: process.env.ELEVENTY_ENV,
-    timestamp: new Date()
+const childProcess = require("child_process")
+
+const getLatestGitCommitHash = (format = "long") => {
+    return childProcess
+        .execSync(`git rev-parse ${format === "short" ? "--short" : ""} HEAD`)
+        .toString()
+        .trim()
+}
+
+module.exports = () => {
+    const latestGitCommitHash = getLatestGitCommitHash("short")
+
+    return {
+        env: process.env.ELEVENTY_ENV,
+        timestamp: new Date(),
+        hash: latestGitCommitHash
+    }
 }
