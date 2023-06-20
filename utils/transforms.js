6bcbc78ab84afb9e3672268500b2b3b173759771
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-20 11:13:04 +0700

    try to remove critical

--- utils/transforms.js
+++ utils/transforms.js
@@ -6,8 +6,6 @@ const shouldTransformHTML = (outputPath) =>
     outputPath.endsWith(".html") &&
     process.env.ELEVENTY_ENV === "production"
 
-const isHomePage = (outputPath) => outputPath === `${buildDir}/index.html`
-
 process.setMaxListeners(Infinity)
 module.exports = {
     htmlmin: function (content, outputPath) {
@@ -17,26 +15,6 @@ module.exports = {
                 removeComments: true,
                 collapseWhitespace: true
             })
-        }
-        return content
-    },
-
-    critical: async function (content, outputPath) {
-        if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
-            try {
-                const config = {
-                    base: `${buildDir}/`,
-                    html: content,
-                    inline: true,
-                    width: 1280,
-                    height: 800
-                }
-                const { generate } = await import("critical")
-                const { html } = await generate(config)
-                return html
-            } catch (err) {
-                console.error(err)
-            }
         }
         return content
     }
