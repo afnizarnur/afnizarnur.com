9f535da21786e0ce30cf61e693478dc06ade19ec
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-21 09:25:27 +0700

    update things

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,7 +4,6 @@ import "focus-visible"
 // Internal Modules
 import "./modules/nav"
 import "./modules/themepicker"
-import "./modules/cursor"
 import "./modules/clipboard"
 import "./modules/local-time"
 import "./modules/spotify"
