aae2714451083b94ef1d188e5fc77f72b6ea9d3e
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-23 18:18:08 +0700

    Add stagger animation on design tools

--- src/assets/scripts/modules/animation.js
+++ src/assets/scripts/modules/animation.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
         createScrollTrigger(element, tl)
     })
 
-    // Get all the div elements with the class "selected-work--item"
+    // Animation on selected work
     const selectedWorkItems = document.querySelectorAll(".selected-work--item");
 
     selectedWorkItems.forEach((item, index) => {
@@ -54,6 +54,39 @@ document.addEventListener("DOMContentLoaded", function () {
             onEnter: () => timeline.play(), 
         });
     });
+
+        // Animation on selected work
+        const designToolingCards = document.querySelectorAll(".design-tooling-card");
+        // Create a GSAP timeline for each row
+        const timelines = [];
+      
+        // Iterate over each design tooling card and create timelines for each row
+        designToolingCards.forEach((card, index) => {
+          const rowIndex = Math.floor(index / 2); // Calculate the row index based on the card index
+      
+          if (!timelines[rowIndex]) {
+            // If a timeline for the row doesn't exist, create a new timeline
+            timelines[rowIndex] = gsap.timeline({ paused: true });
+          }
+      
+          // Add the card to the corresponding row timeline
+          timelines[rowIndex].fromTo(
+            card,
+            { opacity: 0 },
+            { opacity: 1, duration: 0.5 },
+            index % 2 === 0 ? 0 : 0.2 // Staggered delay within the row
+          );
+        });
+      
+        // Create a scroll trigger for each row timeline
+        timelines.forEach((timeline, index) => {
+          const rowStart = index === 0 ? "top 90%" : `top+=${index * 10}%`; // Start the animation when the trigger element is 80% in view for the first row, and staggered for subsequent rows
+          ScrollTrigger.create({
+            trigger: designToolingCards[index * 2], // Trigger element (first card in the row)
+            start: rowStart,
+            onEnter: () => timeline.play(), // Play the timeline animation when the trigger element enters the viewport
+          });
+        });
 
     // Avoid flash of unstyled content
     const textSplitElements = document.querySelectorAll("[text-split]")
