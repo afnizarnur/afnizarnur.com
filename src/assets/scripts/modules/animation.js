9f2c3ff556500207592eb4119878ad3d7da8a770
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-23 21:47:22 +0700

    Make it the code readable and dry

--- src/assets/scripts/modules/animation.js
+++ src/assets/scripts/modules/animation.js
@@ -4,48 +4,40 @@ import SplitType from "split-type"
 
 gsap.registerPlugin(ScrollTrigger)
 
-function createScrollTrigger(triggerElement, timeline) {
-    ScrollTrigger.create({
-        trigger: triggerElement,
-        start: "top 90%",
-        onEnter: () => timeline.play()
-    })
-}
-
-function isMobileOrTablet() {
-    return window.matchMedia("(max-width: 1024px)").matches
-}
-
 document.addEventListener("DOMContentLoaded", function () {
     const typeSplit = new SplitType("[text-split]", {
         types: "words, chars",
         tagName: "span"
     })
 
-    // Animation text reveal
-    document.querySelectorAll("[letters-slide-up]").forEach(function (element) {
+    function createScrollTrigger(triggerElement, timeline) {
+        ScrollTrigger.create({
+            trigger: triggerElement,
+            start: "top 90%",
+            onEnter: () => timeline.play()
+        })
+    }
+
+    function animateTextReveal(element) {
         const tl = gsap.timeline({ paused: true })
         const chars = element.querySelectorAll(".char")
 
         tl.from(chars, {
             yPercent: 100,
-            duration: 0.2,
+            duration: 0.4,
             ease: "power1.out",
             stagger: { amount: 0.6 }
         })
 
         createScrollTrigger(element, tl)
-    })
+    }
 
-    // Animation on selected work
-    const selectedWorkItems = document.querySelectorAll(".selected-work--item")
-
-    selectedWorkItems.forEach((item, index) => {
+    function animateSelectedWork(item) {
         const timeline = gsap.timeline({ paused: true })
         timeline.fromTo(
             item,
             { opacity: 0, y: 50 },
-            { opacity: 1, y: 0, duration: 0.5 }
+            { opacity: 1, y: 0, duration: 0.4 }
         )
 
         ScrollTrigger.create({
@@ -53,35 +45,47 @@ document.addEventListener("DOMContentLoaded", function () {
             start: "top 90%",
             onEnter: () => timeline.play()
         })
-    })
+    }
+
+    function animateDesignToolingCards(cards) {
+        const timelines = []
+
+        cards.forEach((card, index) => {
+            const rowIndex = Math.floor(index / 2)
+
+            if (!timelines[rowIndex]) {
+                timelines[rowIndex] = gsap.timeline({ paused: true })
+            }
+
+            timelines[rowIndex].fromTo(
+                card,
+                { opacity: 0 },
+                { opacity: 1, duration: 0.4 },
+                index % 2 === 0 ? 0 : 0.2 // Staggered delay within the row
+            )
+        })
+
+        timelines.forEach((timeline, index) => {
+            const rowStart = index === 0 ? "top 90%" : `top+=${index * 10}%`
+            ScrollTrigger.create({
+                trigger: cards[index * 2],
+                start: rowStart,
+                onEnter: () => timeline.play()
+            })
+        })
+    }
+
+    // Animation text reveal
+    const textRevealElements = document.querySelectorAll("[letters-slide-up]")
+    textRevealElements.forEach(animateTextReveal)
 
     // Animation on selected work
+    const selectedWorkItems = document.querySelectorAll(".selected-work--item")
+    selectedWorkItems.forEach(animateSelectedWork)
+
+    // Animation on design tooling cards
     const designToolingCards = document.querySelectorAll(".design-tooling-card")
-    const timelines = []
-
-    designToolingCards.forEach((card, index) => {
-        const rowIndex = Math.floor(index / 2)
-
-        if (!timelines[rowIndex]) {
-            timelines[rowIndex] = gsap.timeline({ paused: true })
-        }
-
-        timelines[rowIndex].fromTo(
-            card,
-            { opacity: 0 },
-            { opacity: 1, duration: 0.5 },
-            index % 2 === 0 ? 0 : 0.2 // Staggered delay within the row
-        )
-    })
-
-    timelines.forEach((timeline, index) => {
-        const rowStart = index === 0 ? "top 90%" : `top+=${index * 10}%`
-        ScrollTrigger.create({
-            trigger: designToolingCards[index * 2],
-            start: rowStart,
-            onEnter: () => timeline.play()
-        })
-    })
+    animateDesignToolingCards(designToolingCards)
 
     // Avoid flash of unstyled content
     const textSplitElements = document.querySelectorAll("[text-split]")
