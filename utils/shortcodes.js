38b38da2276e1aa95584bb9a8d09027b9b674660
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2023-06-24 08:58:47 +0700

    Update focus-visible, footer accessibility, and shortcodes prettier

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,45 +2,45 @@ const Image = require("@11ty/eleventy-img")
 const cheerio = require("cheerio")
 
 module.exports = {
-    icon: function (name) {
-        return `<svg class="icon icon--${name}" role="img" aria-hidden="true">
+	icon: function (name) {
+		return `<svg class="icon icon--${name}" role="img" aria-hidden="true">
                     <use xlink:href="#svg-${name}"></use>
                 </svg>`
-    },
-    image: async function (
-        src,
-        alt,
-        sizes = "(max-width: 480px) 100vw, (max-width: 768px) 90vw, 1280px",
-        width,
-        height
-    ) {
-        let path = "src/assets/images/" + src
-        console.log(`Generating image(s) from: ${path}`)
+	},
+	image: async function (
+		src,
+		alt,
+		sizes = "(max-width: 480px) 100vw, (max-width: 768px) 90vw, 1280px",
+		width,
+		height
+	) {
+		let path = "src/assets/images/" + src
+		console.log(`Generating image(s) from: ${path}`)
 
-        let metadata = await Image(path, {
-            widths: [480, 768, "auto"],
-            formats: ["AVIF"],
-            urlPath: "/assets/images/",
-            outputDir: "./dist/assets/images/"
-        })
+		let metadata = await Image(path, {
+			widths: [480, 768, "auto"],
+			formats: ["AVIF"],
+			urlPath: "/assets/images/",
+			outputDir: "./dist/assets/images/"
+		})
 
-        let imageAttributes = {
-            alt,
-            sizes,
-            loading: "lazy",
-            decoding: "async"
-        }
+		let imageAttributes = {
+			alt,
+			sizes,
+			loading: "lazy",
+			decoding: "async"
+		}
 
-        let imageHTML = Image.generateHTML(metadata, imageAttributes)
+		let imageHTML = Image.generateHTML(metadata, imageAttributes)
 
-        if (width && height) {
-            let $ = cheerio.load(imageHTML)
-            let imgElement = $("img")
-            imgElement.attr("width", width)
-            imgElement.attr("height", height)
-            imageHTML = $.html()
-        }
+		if (width && height) {
+			let $ = cheerio.load(imageHTML)
+			let imgElement = $("img")
+			imgElement.attr("width", width)
+			imgElement.attr("height", height)
+			imageHTML = $.html()
+		}
 
-        return imageHTML
-    }
+		return imageHTML
+	}
 }
