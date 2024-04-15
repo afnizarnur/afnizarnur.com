ee9e74b76bfcfa018fc03ec1a640cfbb41b35535
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2024-04-15 20:58:51 +0700

    Remove unnecessary server options from .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -61,41 +61,6 @@ module.exports = function (config) {
 		})
 	)
 
-	config.setServerOptions({
-		// Default values are shown:
-
-		// Whether the live reload snippet is used
-		liveReload: true,
-
-		// Whether DOM diffing updates are applied where possible instead of page reloads
-		domDiff: true,
-
-		// The starting port number
-		// Will increment up to (configurable) 10 times if a port is already in use.
-		port: 8080,
-
-		// Additional files to watch that will trigger server updates
-		// Accepts an Array of file paths or globs (passed to `chokidar.watch`).
-		// Works great with a separate bundler writing files to your output folder.
-		// e.g. `watch: ["_site/**/*.css"]`
-		watch: [],
-
-		// Show local network IP addresses for device testing
-		showAllHosts: true,
-
-		// Use a local key/certificate to opt-in to local HTTP/2 with https
-		https: {
-			// key: "./localhost.key",
-			// cert: "./localhost.cert",
-		},
-
-		// Change the default file encoding for reading/serving files
-		encoding: "utf-8",
-
-		// Show the dev server version number on the command line
-		showVersion: false
-	})
-
 	// Layouts
 	config.addLayoutAlias("base", "base.njk")
 	config.addLayoutAlias("post", "post.njk")
