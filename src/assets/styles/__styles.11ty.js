ad32fedc270f3b9db1ad809b4ac19a41027610f8
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2024-04-26 16:45:44 +0700

    Add support for social.scss and update CSS rendering

--- src/assets/styles/__styles.11ty.js
+++ src/assets/styles/__styles.11ty.js
@@ -3,60 +3,63 @@
 
 // main entry point name
 const ENTRY_FILE_NAME = "main.scss"
+const SOCIAL_FILE_NAME = "social.scss"
 
 const path = require("path")
 const sass = require("node-sass")
 const CleanCSS = require("clean-css")
 const cssesc = require("cssesc")
 const isProd = process.env.ELEVENTY_ENV === "production"
+const fs = require("fs-extra")
 
 module.exports = class {
-    async data() {
-        const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
-        return {
-            permalink: `/assets/styles/main.css`,
-            eleventyExcludeFromCollections: true,
-            entryPath
-        }
-    }
+	async data() {
+		const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`)
+		const socialPath = path.join(__dirname, `/${SOCIAL_FILE_NAME}`)
+		return {
+			permalink: [`/assets/styles/main.css`, `/assets/styles/social.css`],
+			eleventyExcludeFromCollections: true,
+			entryPath,
+			socialPath
+		}
+	}
+	// Compile Sass to CSS,
+	// Embed Source Map in Development
+	async compile(config) {
+		return new Promise((resolve, reject) => {
+			if (!isProd) {
+				config.sourceMap = true
+				config.sourceMapEmbed = true
+				config.outputStyle = "compressed"
+			}
+			return sass.render(config, (err, result) => {
+				if (err) {
+					return reject(err)
+				}
+				resolve(result.css.toString())
+			})
+		})
+	}
 
-    // Compile Sass to CSS,
-    // Embed Source Map in Development
-    async compile(config) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                config.sourceMap = true
-                config.sourceMapEmbed = true
-                config.outputStyle = "compressed"
-            }
-            return sass.render(config, (err, result) => {
-                if (err) {
-                    return reject(err)
-                }
-                resolve(result.css.toString())
-            })
-        })
-    }
+	// Minify & Optimize with CleanCSS in Production
+	async minify(css) {
+		return new Promise((resolve, reject) => {
+			if (!isProd) {
+				resolve(css)
+			}
+			const minified = new CleanCSS().minify(css)
+			if (!minified.styles) {
+				return reject(minified.error)
+			}
+			resolve(minified.styles)
+		})
+	}
 
-    // Minify & Optimize with CleanCSS in Production
-    async minify(css) {
-        return new Promise((resolve, reject) => {
-            if (!isProd) {
-                resolve(css)
-            }
-            const minified = new CleanCSS().minify(css)
-            if (!minified.styles) {
-                return reject(minified.error)
-            }
-            resolve(minified.styles)
-        })
-    }
-
-    // display an error overlay when CSS build fails.
-    // this brilliant idea is taken from Mike Riethmuller / Supermaya
-    // @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-scss.js
-    renderError(error) {
-        return `
+	// display an error overlay when CSS build fails.
+	// this brilliant idea is taken from Mike Riethmuller / Supermaya
+	// @see https://github.com/MadeByMike/supermaya/blob/master/site/utils/compile-scss.js
+	renderError(error) {
+		return `
         /* Error compiling stylesheet */
         *,
         *::before,
@@ -95,25 +98,26 @@ module.exports = class {
             border: solid 2px red;
             position: fixed;
         }`
-    }
+	}
 
-    // render the CSS file
-    async render({ entryPath }) {
-        try {
-            const css = await this.compile({ file: entryPath })
-            const result = await this.minify(css)
-            return result
-        } catch (err) {
-            // if things go wrong
-            if (isProd) {
-                // throw and abort in production
-                throw new Error(err)
-            } else {
-                // otherwise display the error overlay
-                console.error(err)
-                const msg = err.formatted || err.message
-                return this.renderError(msg)
-            }
-        }
-    }
+	// render the CSS file
+	async render({ entryPath, socialPath }) {
+		try {
+			const mainCSS = await this.compile({ file: entryPath })
+			const socialCSS = await this.compile({ file: socialPath })
+
+			await fs.outputFile("./dist/assets/styles/main.css", mainCSS)
+			await fs.outputFile("./dist/assets/styles/social.css", socialCSS)
+
+			return ""
+		} catch (err) {
+			if (isProd) {
+				throw new Error(err)
+			} else {
+				console.error(err)
+				const msg = err.formatted || err.message
+				return this.renderError(msg)
+			}
+		}
+	}
 }
