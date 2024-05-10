5f0859e930cd8862245e833f7f30630f456d4eae
Afnizar Nur Ghifari <afnizarhilmi@gmail.com>
2024-05-10 09:42:57 +0700

    Update meta tags and screenshot wait time

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -63,6 +63,6 @@ module.exports = {
 			`https://afnizarnur.com/social-card${this.page.url}`
 		)
 		const cacheKey = `_${new Date().valueOf()}`
-		return `https://v1.screenshot.11ty.dev/${encodedURL}/opengraph/${cacheKey}/_wait:2/`
+		return `https://v1.screenshot.11ty.dev/${encodedURL}/opengraph/${cacheKey}/_wait:3/`
 	}
 }
